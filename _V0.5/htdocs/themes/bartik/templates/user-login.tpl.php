8d44fe9ebfe1c9a73eee22cccbd2971f42ba59a8
Aurels76 <lehavrai76@hotmail.fr>
2013-07-03 09:48:04 -0700

    Ajout version 5 : remise en place du captcha

---
+++ _V0.5/htdocs/themes/bartik/templates/user-login.tpl.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+<form action="/contact?destination=node/4" method="post" id="user-login-form" accept-charset="UTF-8"><div><div class="form-item form-type-textfield form-item-name">
+  <label for="edit-name">Pseudo <span class="form-required" title="Champ obligatoire.">*</span></label>
+ <input type="text" title="Veuillez saisir votre pseudo" id="edit-name" name="name" value="" size="15" maxlength="60" class="form-text required">
+</div>
+<div class="form-item form-type-password form-item-pass">
+  <label for="edit-pass">Mot de passe <span class="form-required" title="Champ obligatoire.">*</span></label>
+ <input type="password" title="Veuillez saisir votre mot de passe" id="edit-pass" name="pass" size="15" maxlength="128" class="form-text required">
+</div>
+<div class="item-list"><ul><li class="first"><a href="/user/register" title="Create a new user account.">Create new account</a></li>
+<li class="last"><a href="/user/password" title="Request new password via e-mail.">Request new password</a></li> 
+</ul></div>
+<input type="hidden" name="form_build_id" value="form-XzRG2ZU2gQ9jBi-8x-dVo99DzmMB45gs0gElpswc4-A">
+<input type="hidden" name="form_id" value="user_login_block">
+<div class="form-actions form-wrapper" id="edit-actions"><input type="submit" id="edit-submit" title="Se connecter" name="op" value="Se connecter" class="form-submit"></div></div></form>
