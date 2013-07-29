2f15ccebe35b5c7b3637e093f3492b99ca277f7d
Aurels76 <lehavrai76@hotmail.fr>
2013-07-29 13:02:11 -0700

    Ajout de la version 13 : ajout des flux rss

---
+++ _V0.13/public_html/sites/all/modules/custom/aurelwebsite/js/aurelwebsite.js
@@ -0,0 +1,20 @@
+(function($) {
+/*Drupal.behaviors.myBehavior = {
+  attach: function (context, settings) {
+
+    //code starts
+    $("body").click(function() {
+      alert("Hello World");
+    });
+    //code ends
+
+  }
+};*/
+
+jQuery(document).ready(function($) {
+  // Code that uses jQuery's $ can follow here.
+   /*$("body").click(function() {
+      alert("Non mais allo quoi !!");
+    });*/
+});
+})(jQuery);
---
+++ _V0.13/public_html/sites/all/themes/bluemasters/block--user--login.tpl.php
@@ -0,0 +1,13 @@
+<form action="/contact?destination=node/4" method="post" id="user-login-form" accept-charset="UTF-8"><div><div class="form-item form-type-textfield form-item-name">
+  <label for="edit-name">Pseudo <span class="form-required" title="Champ obligatoire.">*</span></label>
+ <input type="text" title="Veuillez saisir votre pseudo" id="edit-name" name="name" value="" size="15" maxlength="60" class="form-text required">
+</div>
+<div class="form-item form-type-password form-item-pass">
+  <label for="edit-pass">Mot de passe <span class="form-required" title="Champ obligatoire.">*</span></label>
+ <input type="password" title="Veuillez saisir votre mot de passe" id="edit-pass" name="pass" size="15" maxlength="128" class="form-text required">
+</div>
+<br>
+<input type="hidden" name="form_build_id" value="form-XzRG2ZU2gQ9jBi-8x-dVo99DzmMB45gs0gElpswc4-A">
+<input type="hidden" name="form_id" value="user_login_block">
+<div class="form-actions form-wrapper" id="edit-actions"><input type="submit" id="edit-submit" title="Se connecter" name="op" value="Se connecter" class="form-submit"></div></div></form>
+
